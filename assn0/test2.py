7503c93a9829823eae77c671eda6786cbc842c08
JasmineSun <2043281367@qq.com>
2018-07-04 21:51:24 +0800

    add

--- assn0/test2.py
+++ assn0/test2.py
@@ -1,22 +1,140 @@
 # -*- coding:utf-8 -*-
 
+# 列表生成式
+# [x*x for x in range(1,10)]
+
+# 两层循环
+# [m + n for m in 'ABC' for n in 'XYZ']
+
+
+
+
+# generator
+s = (x * x for x in range(5))
+print(s)
+for x in s:
+    print(x)
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+f = fib(10)
+print('fib(10):', f)
+for x in f:
+    print(x)
+
+# call generator manually:
+g = fib(5)
+while 1:
+    try:
+        x = next(g)
+        print('g:', x)
+    except StopIteration as e:
+        print('Generator return value:', e.value)
+        break
+
+        
+
+# 迭代
+for i,value in enumerate(['jasmine','fortunate']):
+    print(i,value)
+
+# 利用递归函数计算阶乘
+# N! = 1 * 2 * 3 * ... * N
+def fact(n):
+    if n == 1:
+        return 1
+    return n * fact(n-1)
+
+print('fact(1) =', fact(1))
+print('fact(5) =', fact(5))
+print('fact(10) =', fact(10))
+
+# 利用递归函数移动汉诺塔:
+def move(n, a, b, c):
+    if n == 1:
+        print('move', a, '-->', c)
+    else:
+        move(n-1, a, c, b)
+        move(1, a, b, c)
+        move(n-1, b, a, c)
+
+move(4, 'A', 'B', 'C')
+
+# 递归函数
+def fact_iter(num, product):
+    if num == 1:
+        return product
+    return fact_iter(num - 1, num * product)
+print(fact_iter(5,1))
+
+
+def print_scores(**kw):
+    print('      Name  Score')
+    print('------------------')
+    for name, score in kw.items():
+        print('%10s  %d' % (name, score))
+    print()
+
+print_scores(Adam=99, Lisa=88, Bart=77)
+
+data = {
+    'Adam Lee': 99,
+    'Lisa S': 88,
+    'F.Bart': 77
+}
+
+print_scores(**data)
+
+def print_info(name, *, gender, city='Beijing', age):
+    print('Personal Info')
+    print('---------------')
+    print('   Name: %s' % name)
+    print(' Gender: %s' % gender)
+    print('   City: %s' % city)
+    print('    Age: %s' % age)
+    print()
+
+print_info('Bob', gender='male', age=20)
+print_info('Lisa', gender='female', city='Shanghai', age=18)
+
+
+
+#参数
+def hello(greeting,*args):
+    if (len(args)==0):
+        print('%s!' % greeting)
+    else:
+        print('%s, %s!' % (greeting, ', '.join(args)))
+
+hello('Hi') # => greeting='Hi', args=()
+hello('Hi', 'Sarah') # => greeting='Hi', args=('Sarah')
+hello('Hello', 'Michael', 'Bob', 'Adam') # => greeting='Hello', args=('Michael', 'Bob', 'Adam')
+
+names = ('Bart', 'Lisa')
+hello('Hello', *names) # => greeting='Hello', args=('Bart', 'Lisa')
 
 # 默认参数
 def add_end(L=None):
-	if L is None:
-		L=[]
-	L.append('END')
-	return L
+    if L is None:
+        L=[]
+    L.append('END')
+    return L
 print(add_end())
 
 
 # 计算x的n次方
 def power(x,n):
-	s=1
-	while n>0:
-		n=n-1
-		s=s*x
-	return s
+    s=1
+    while n>0:
+        n=n-1
+        s=s*x
+    return s
 # x=input('输入底数:',x)
 # n=input('输入指数:',n)
 print(power(2,3))
@@ -28,10 +146,10 @@ print(power(2,3))
 # 请定义一个函数quadratic(a, b, c)，接收3个参数，返回一元二次方程
 import math
 def quadratic(a,b,c):
-	x1=(-b+math.sqrt(b*b-4*a*c))/(2*a)
-	x2=(-b-math.sqrt(b*b-4*a*c))/(2*a)
-	# print(x1,x2)
-	return x1,x2
+    x1=(-b+math.sqrt(b*b-4*a*c))/(2*a)
+    x2=(-b-math.sqrt(b*b-4*a*c))/(2*a)
+    # print(x1,x2)
+    return x1,x2
 # 测试:
 
 print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
@@ -78,8 +196,8 @@ def is_float(x):
     except:
         return False
 
-x = input("计算绝对值: ")
-print(my_abs(x))
+# x = input("计算绝对值: ")
+print(my_abs(-7))
 
 
 
---
+++ assn0/test3.py
@@ -0,0 +1,100 @@
+# -*- coding=utf-8 -*-
+
+
+# 继承和多态
+class Animal(object):
+    def run(self):
+        print('Animal is running...')
+
+class Dog(Animal):
+    def run(self):
+        print('Dog is running...')
+
+class Cat(Animal):
+    def run(self):
+        print('Cat is running...')
+
+def run_twice(animal):
+    animal.run()
+    animal.run()
+
+a = Animal()
+d = Dog()
+c = Cat()
+
+print('a is Animal?', isinstance(a, Animal))
+print('a is Dog?', isinstance(a, Dog))
+print('a is Cat?', isinstance(a, Cat))
+
+print('d is Animal?', isinstance(d, Animal))
+print('d is Dog?', isinstance(d, Dog))
+print('d is Cat?', isinstance(d, Cat))
+
+run_twice(c)
+
+
+
+# 类
+class Student(object):
+# 特殊方法“__init__”前后分别有两个下划线
+# 注意到__init__方法的第一个参数永远是self，表示创建的实例本身，因此，在__init__方法内部，就可以把各种属性绑定到self，因为self就指向创建的实例本身
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score(self):
+        print('%s: %s' % (self.name, self.score))
+
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >= 60:
+            return 'B'
+        else:
+            return 'C'
+
+bart = Student('Bart Simpson', 59)
+lisa = Student('Lisa Simpson', 87)
+
+print('bart.name =', bart.name)
+print('bart.score =', bart.score)
+bart.print_score()
+
+print('grade of Bart:', bart.get_grade())
+print('grade of Lisa:', lisa.get_grade())
+
+
+# 访问限制
+class Student(object):
+
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+
+    def set_score(self, score):
+        if 0 <= score <= 100:
+            self.__score = score
+        else:
+            raise ValueError('bad score')
+
+    def get_grade(self):
+        if self.__score >= 90:
+            return 'A'
+        elif self.__score >= 60:
+            return 'B'
+        else:
+            return 'C'
+
+bart = Student('Bart Simpson', 59)
+print('bart.get_name() =', bart.get_name())
+bart.set_score(60)
+print('bart.get_score() =', bart.get_score())
+
+print('DO NOT use bart._Student__name:', bart._Student__name)
+
